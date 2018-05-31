26869a46a4e6ea09efd283e1986b766ac3155c54
Erika Ramírez <00157516@uca.edu.sv>
2018-05-30 23:18:29 -0600

    Initial commit

---
+++ app/src/main/java/com/ramirez/proyecto/MainActivity.java
@@ -0,0 +1,29 @@
+package com.ramirez.proyecto;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent nueva = new Intent(getApplicationContext(), Main2Activity.class);
+                startActivity(nueva);
+            }
+        });
+
+    }
+}
