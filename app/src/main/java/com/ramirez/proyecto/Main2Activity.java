0fefef39e53e30ae25446dfab040ae900db8fc17
Erika Ramírez <00157516@uca.edu.sv>
2018-07-01 17:52:37 -0600

    xml nuevos

--- app/src/main/java/com/ramirez/proyecto/Main2Activity.java
+++ app/src/main/java/com/ramirez/proyecto/Main2Activity.java
@@ -80,17 +80,17 @@ public class Main2Activity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.login) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.menu) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.info) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.facebook) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.whats) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.instagram) {
 
         }
 
