67319c7f8bcc6a1d293fae1f3678c4ee1151d90c
maxisun <00354615@uca.edu.sv>
2018-07-04 15:22:58 -0600

    app.js fill

--- app.js
+++ app.js
@@ -7,6 +7,28 @@ const mongoose = require('mongoose');
 //variables
 require('dotenv').config();
 
+//conexion a la base de datos, desde el link que nos proporciono la base de datos en ATLAS.
+mongoose.connect('mongodb+srv://maxisun:'+process.env.MONGO_ATLAS_PASSWORD+'@rep-sazon-im8dq.mongodb.net/test?retryWrites=true');
+mongoose.Promise = global.Promise;
+
+//configurando el uso de bodyParser
+app.use(bodyParser.urlencoded({extended: false}));//parser para bodies de urlencoded al cual no queremos que sean extendidos, por eso el "false"
+app.use(bodyParser.json());//configuracion para bodies que contengan JSON
+
+//manejo de error CORS por medio de Headers
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin','*');//permite cualquier origen para acceder al API
+  //res.header('Access-Control-Allow-Origin','http://peneduro.com');//solo esa direccion  de origen puede acceder
+  res.header("Access-Control-Allow-Headers",
+  "Origin, X-Requested-With, Content-Type, Accept, Authorization");//son los Headers que permitimos para acceder al API
+  //El browser siempre envia esto, asi que le indicamos que metodos estan disponibles para el uso de la API
+  if (req.method === 'OPTIONS'){
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, PATCH');//protocolos Http que soporta esta API
+    return res.status(200).json({});//se responde que metodos tiene permitido usar al browser o cliente
+  }
+  next();//procedemos a resto de las rutas
+});
+
 //lidiando con errores, si no logro acceder a ninguna de las rutas de arriba
 app.use((req, res, next) => {
   const error = new Error('Not found');
