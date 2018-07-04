026e1352ae38b5d25d2822c845d2e6aba63bc6fb
maxisun <00354615@uca.edu.sv>
2018-07-04 11:42:54 -0600

    coomit inicial

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+//lidiando con errores, si no logro acceder a ninguna de las rutas de arriba
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);//se pasa a la funcion de abajo
+});
+
+//esta funcion maneja todo tipo de errores
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+module.exports = app;
