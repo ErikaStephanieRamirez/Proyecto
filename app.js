9d97b28710c96ad1308db4764190f01bd65ced28
maxisun <00354615@uca.edu.sv>
2018-07-04 15:08:16 -0600

    estructura de los datos

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+//paquete de body bodyParser
+const bodyParser = require('body-parser');
+//paquete monoose
+const mongoose = require('mongoose');
+//variables
+require('dotenv').config();
 
 //lidiando con errores, si no logro acceder a ninguna de las rutas de arriba
 app.use((req, res, next) => {
